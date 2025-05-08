eef7359a328e2d8a5cca71daef52e1f739782989
MuhammadRioAprilianto <rma994636@gmail.com>
2025-05-08 10:43:27 +0700

    memuat fungsi delNode

--- linkedList.cpp
+++ linkedList.cpp
@@ -69,5 +69,19 @@ class SingleLinkedList
         
         return (*current != NULL);
     }
-    
+
+    bool delNode(int nim)
+    {
+        Node *current, *previous;
+        if (!Search(nim, &previous, &current))
+            return false;
+
+        if (current == START)
+            START = START->next;
+        else
+            previous->next = current->next;
+
+        delete current;
+        return true;
+    }
 };
