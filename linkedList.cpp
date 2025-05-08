acd72f4e7f55c27259b2ecf79d7f2c0c11e83900
MuhammadRioAprilianto <rma994636@gmail.com>
2025-05-08 11:26:07 +0700

    memuat case 5

--- linkedList.cpp
+++ linkedList.cpp
@@ -170,9 +170,18 @@ int main()
                      << "Data tidak ditemukan" << endl;
             else
             {
-                
+                cout << endl
+                     << "Data ditemukan" << endl;
+                cout << "\nNo Mahasiswa: " << current->noMhs << endl;
+                cout << "\n";
             }
         }
+        break;
+        case '5':
+        {
+            exit(0);
+        }
+        
         }
 
     }
